1f14fe00049f53281254237f3ff1c64ee038ff2b
fmg75 <py.fmg.2022@gmail.com>
2023-06-30 09:03:41 -0300

    pkl

--- app3.py
+++ app3.py
@@ -161,19 +161,8 @@ def run_feature_extraction(uploaded_files, data_dir):
             st.write("Diccionario de características:")
             st.write(caracteristicas)
 
-            # Generar el nombre de archivo temporal
-            # _, temp_filename = tempfile.mkstemp(suffix=".pkl")
-
-            # # Guardar el diccionario de características en un archivo temporal
-            # with open(temp_filename, "wb") as f:
-            #     pickle.dump(caracteristicas, f)
-
-            filename = data_dir + "/feature_" + unique_id + ".pkl"
+            filename = f"{data_dir}/feature_{unique_id}.pkl"
             filename = filename.replace("/", "\\")
-            # shutil.copy(temp_filename, filename)
-            # Guardar el diccionario de características en un archivo
-            # with open(filename, "wb") as f:
-            #     pickle.dump(caracteristicas, f)
 
             # Botón de descarga
             with open(filename, "wb") as f:
@@ -184,7 +173,7 @@ def run_feature_extraction(uploaded_files, data_dir):
                     "Haz clic aquí para descargar el archivo de características",
                 )
 
-            st.write(f"Diccionario de características guardado en {filename}")
+        # st.write(f"Diccionario de características guardado en {filename}")
         except Exception as e:
             st.error("Ocurrió un error. Detalles: " + str(e))
 
